3af64040cdfb2d0ebbff543f50a1f7143d8e8c6f
CHOI_MINGI <alrltjd1810@gmail.com>
2026-04-11 21:52:24 +0900

    [Bronze II] Title: 16진수, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

--- Java/백준/Bronze/1550. 16진수/16진수.java
+++ Java/백준/Bronze/1550. 16진수/16진수.java
@@ -2,10 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        String hex = sc.nextLine();
 
-        String num =  input.nextLine();
-        int result = Integer.parseInt(num, 16);
+        int result = 0;
+
+        for (int i = 0; i < hex.length(); i++) {
+            char c = hex.charAt(i);
+            int value;
+
+            if (c >= '0' && c <= '9') {
+                value = c - '0';
+            } else if (c >= 'A' && c <= 'F') {
+                value = c - 'A' + 10;
+            } else if (c >= 'a' && c <= 'f') {
+                value = c - 'a' + 10;
+            } else {
+                return;
+            }
+
+            result = result * 16 + value;
+        }
+
         System.out.println(result);
     }
 }
